1d5e859c28a0a12019de3c860739f60d370b85dd
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-03-20 09:16:02 -0600

    Server Up

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Server Up" });
+});
+
+module.exports = server;
